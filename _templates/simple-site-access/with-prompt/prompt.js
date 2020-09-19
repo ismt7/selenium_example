8df804225ce51c4089b03b945490927b878cc96a
ismt7 <tatuya.0807.t@gmail.com>
2020-09-19 23:33:32 +0900

    コードジェネレーターを追加

---
+++ _templates/simple-site-access/with-prompt/prompt.js
@@ -0,0 +1,11 @@
+// see types of prompts:
+// https://github.com/enquirer/enquirer/tree/master/examples
+//
+module.exports = [
+  {
+    type: 'input',
+    name: 'file_name',
+    message: "File name?",
+    initial: 'test',
+  }
+]
