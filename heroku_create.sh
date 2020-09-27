cd2806810585cd7f42ced8caf9038ff2838d35d5
Tatsuya Ishimoto <tatuya.0807.t@gmail.com>
2020-09-27 21:45:04 +0900

    herokuアプリ作成シェルを対話形式でアプリ名を受け取るようにする#12

--- heroku_create.sh
+++ heroku_create.sh
@@ -1,6 +1,16 @@
-export APP_NAME={アプリ名}
+#!/bin/bash
+
+while true
+do
+read -p "What is the application name?: " app_name
+if test -n "$app_name"
+then
+    break
+fi
+done
+
 heroku container:login
-heroku create $APP_NAME --manifest
-heroku git:remote -a $APP_NAME
+heroku create $app_name --manifest
+heroku git:remote -a $app_name
 heroku container:push selenium
 heroku container:release selenium
